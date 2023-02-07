45029750341c63a0b1c4628fe199111f94a92549
karthika0106 <81954901+karthika0106@users.noreply.github.com>
2023-02-07 14:42:37 +0530

    Add files via upload

---
+++ looping 001.java
@@ -0,0 +1,10 @@
+public class day1 {
+	public static void main(String[] args) {
+		for(int i=100;i>=0;i=i-3) {
+			System.out.print(i);
+			System.out.print(" ");
+			
+		}
+	}
+
+}
---
+++ looping infinte (1).java
@@ -0,0 +1,10 @@
+public class day1 {
+	public static void main(String[] args) {
+		for(int i=100;i>=0;i--) {
+			System.out.print(i);
+			System.out.print(" ");
+			
+		}
+	}
+
+}
